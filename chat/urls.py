4b22fd782700069a9978dd151674a2ae4672cbeb
Pavel <pavel@pavel-Inspiron-N5110.(none)>
2013-01-29 12:02:18 +0200

    removing dajaxice

--- chat/urls.py
+++ chat/urls.py
@@ -3,14 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
-dajaxice_autodiscover()
-
 urlpatterns = patterns('',
     url(r'^', include('app.urls')),
     # Admins:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    # ajax:
-    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 )
